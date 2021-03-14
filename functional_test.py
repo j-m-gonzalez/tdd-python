8aac9f822a5b5cadbb0c906355b3b77a9b01d299
j-m-gonzalez <jgonzalez4927@gmail.com>
2021-03-14 14:01:20 -0700

    FT refactor

--- functional_test.py
+++ functional_test.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Willy checks out homepage
         self.browser.get('http://localhost:8000')
@@ -32,12 +37,16 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Make fudge"
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Make fudge')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Eat fudge')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
+        #Page updates again, shows both items on list
+        self.check_for_row_in_list_table('1: Make fudge')
+        self.check_for_row_in_list_table('2: Eat fudge')
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn('1: Make fudge', [row.text for row in rows])
